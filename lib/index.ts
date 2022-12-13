bf918a770d24213b2f8d4df6fcaa6d914a9b2842
Pedro Henrique Kopper <pedro.kopper@balena.io>
2022-12-13 11:49:06 -0300

    Add apidoc boilerplate
    Signed-off-by: Pedro Henrique Kopper <pedro.kopper@balena.io>

--- lib/index.ts
+++ lib/index.ts
@@ -14,6 +14,31 @@ app.use('/api/network', networkRoutes.default);
 app.use('/api/serial', serialRoutes.default);
 app.use('/api/video', videoRoutes.default);
 
+/**
+ * @api {get} /user/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *       "firstname": "John",
+ *       "lastname": "Doe"
+ *     }
+ *
+ * @apiError UserNotFound The id of the User was not found.
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 404 Not Found
+ *     {
+ *       "error": "UserNotFound"
+ *     }
+ */
 app.get('/', (req: express.Request, res: express.Response) => {
 	res.send('Hello World!');
 });
